510ce04576f794e73fa31895474e5c9469c0ca1d
Matteo Di Mario <mdima0012@gmail.com>
2023-09-27 17:51:40 +0200

    Added parser.

--- StripeLine-Pointing-Simulation.jl
+++ StripeLine-Pointing-Simulation.jl
@@ -1,6 +1,7 @@
 import Stripeline
-import Printf
 
+using Printf
+using ArgParse
 
 
 # =========================
@@ -8,3 +9,40 @@ import Printf
 # to study the pointing error distribution when there are non idealities in the telescope.
 # It should be used with GNU parallel to perform compelte simualtion of 2 yrs and all 49 polarimeters
 # =========================
+
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+
+        "start-day"
+            help = "The starting day of the simualtion."
+            arg_type = Int
+            required = true
+
+        "lenght"
+            help = "How many days the simulation will last."
+            arg_type = Int
+            required = true
+
+        "polarimeter"
+            help = "The polarimeter to simulate."
+            arg_type = String
+            required = true
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println("All clear!")
+
+    println("Parsed args:")
+    for (arg,val) in parsed_args
+        println("  $arg  =>  $val")
+    end
+end
+
+main()
