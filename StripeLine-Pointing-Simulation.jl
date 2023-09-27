37d21025e80ae189d470b308ba4b446b47d0e1b8
Matteo Di Mario <mdima0012@gmail.com>
2023-09-27 09:54:09 +0200

    First commit

---
+++ StripeLine-Pointing-Simulation.jl
@@ -0,0 +1,10 @@
+import Stripeline
+import Printf
+
+
+
+# =========================
+# This code performs a simulation for a single polarimeter of the Stripeline pipeline,
+# to study the pointing error distribution when there are non idealities in the telescope.
+# It should be used with GNU parallel to perform compelte simualtion of 2 yrs and all 49 polarimeters
+# =========================
