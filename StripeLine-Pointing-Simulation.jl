026c8e9ac9d7c0a80da56ff79927071c1c6fc9c4
Matteo Di Mario <mdima0012@gmail.com>
2023-09-28 15:54:51 +0200

    Added histogram make and fill function

--- StripeLine-Pointing-Simulation.jl
+++ StripeLine-Pointing-Simulation.jl
@@ -2,6 +2,7 @@ import Stripeline
 
 using Printf
 using ArgParse
+using AstroLib, Dates
 
 
 # =========================
@@ -35,14 +36,54 @@ function parse_commandline()
     return parse_args(s)
 end
 
+function make_histogram(θ, nbins)
+    
+    H =zeros(Float64, nbins, 2)
+    step = θ / nbins
+
+    for i in range(1,nbins)
+        H[i,2] = -2*θ + (2*i - 1)/2 * step
+    end
+
+    return H, step
+
+end
+
+function fill_histogram!(H, nbins, step, point_errs)
+
+    θ = step * nbins
+
+    for i in range(1, length(point_errs))
+
+        count = 0
+        while true
+            j = ceil(length(H[:,2]) / 2)
+            rbin = H[j, 2] + step
+            lbin = H[j,2] - step
+
+            if lbin < point_errs[i] < rbin
+                H[i,1]+=1
+                break
+            elseif point_errs[i] > rbin 
+                j += ceil(j/2)
+            else # If number is equal, is in lbin
+                j = ceil(j/2)
+            end
+            count+=1
+            count != nbins / 2 || break
+        end
+    end
+
+
+end
+
 function main()
 
     parsed_args = parse_commandline()
 
-    # Simualtion parameters 
+    # Simulation parameters 
     start_day = parsed_args["start_day"]
     length = parsed_args["length"]
-    end_day = start_day + length
     pol_name = parsed_args["polarimeter"]
 
 
@@ -52,10 +93,48 @@ function main()
     fsamp_hz = 50
     τ_s = 1 / fsamp_hz
 
-    println("Simulating polarimeter $(pol_name) from day $(start_day) to day $(end_day)")
+    println("Simulating polarimeter $(pol_name) from day $(start_day) to day $(start_day+length)")
 
 
 
 end
 
 main()
+
+
+
+
+# time_range = 0 : τ_s : (total_time_s - τ_s)
+# telescope_motors(time_s) = (0.0, deg2rad(20.0), Stripeline.timetorotang(time_s, 1.))
+
+# (dirs, ψ) = Stripeline.genpointings(
+#     telescope_motors,
+#     [0., 0., 1.],
+#     time_range;
+#     config_ang = nothing
+# )
+
+
+# config_ang = Stripeline.configuration_angles(
+#     wheel1ang_0_rad  = deg2rad(0),
+#     wheel2ang_0_rad  = deg2rad(0),
+#     wheel3ang_0_rad  = deg2rad(2.),
+#     forkang_rad  = deg2rad(0),
+#     omegaVAXang_rad  = deg2rad(0),
+#     zVAXang_rad  = deg2rad(0),
+#     panang_rad  = deg2rad(0),
+#     tiltang_rad  = deg2rad(0),
+#     rollang_rad  = deg2rad(0)  
+# ) 
+
+
+# (dirs_config, ψ_config) = Stripeline.genpointings(
+#     telescope_motors,
+#     [0., 0., 1.],
+#     time_range;
+#     config_ang = config_ang 
+# )
+
+# point_err = acos.(  sin.(dirs[:,1]) .* cos.(dirs[:,2]) .* sin.(dirs_config[:,1]) .* cos.(dirs_config[:,2]) .+
+# sin.(dirs[:,1]) .* sin.(dirs[:,2]) .* sin.(dirs_config[:,1]) .* sin.(dirs_config[:,2]) .+
+# cos.(dirs[:,1]) .* cos.(dirs_config[:,1])) .* 3437.75 # Converts to arcmin
