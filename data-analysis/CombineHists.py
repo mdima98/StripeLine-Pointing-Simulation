6ed4a0a5d6ad368985838e46cf278c31196d82d8
Matteo Di Mario <mdima0012@gmail.com>
2023-10-10 10:30:41 +0200

    Deleted deprecated .hist data

---
+++ data-analysis/CombineHists.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import toml
+
+
+
+
+
+def main():
+    
+    fpath1 = str(sys.argv[1])
+    fpath2 = str(sys.argv[2])
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    main()
