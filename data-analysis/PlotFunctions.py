4f7893f7e4464e534aedf48660aa3bada398aa58
Matteo Di Mario <mdima0012@gmail.com>
2023-10-30 22:17:50 +0100

    Updated plotting functions

--- data-analysis/PlotFunctions.py
+++ data-analysis/PlotFunctions.py
@@ -5,21 +5,21 @@ def plot_hist(specifics, fhist):
     
     fig, ax = plt.subplots(figsize=(8, 6), tight_layout=True)
     
-    (bins, freq) = np.loadtxt(fhist, delimiter=',', unpack=True, dtype=int)
+    hist = np.loadtxt(fhist, delimiter=',', unpack=False, dtype=int)
     
-    # Sort for hist plotting  
-    # bins = np.sort(bins)
-    # freq = np.array([freq for _, freq in sorted(zip(bins, freq), key=lambda pair: pair[0])])
-    
+    # Sort (bins,freq) for hist plotting  
+    hist = np.array(sorted(hist, key=lambda x: x[0]))
+    bins = hist[:,0]
+    freq = hist[:,1]
+
     bins_edges = np.append(bins-0.5, bins[-1]+0.5)
     
-    # ax.stairs(freq, bins_edges, edgecolor="b", linewidth=1.0, fill=False, label=specifics["pol_name"])
-    ax.bar(bins, freq, width=1.0,align='center', label=specifics["pol_name"])
+    ax.stairs(freq, bins_edges, edgecolor="b", linewidth=1.0, fill=False, label=specifics["pol_name"])
     
     ax.set_title("Pointing Error Distribution")
     ax.set_xlabel(f"Pointing Error [{specifics['units']}]")
     ax.set_yscale("log")
-    ax.set_ylabel('Frequency')
+    ax.set_ylabel('Count')
     ax.legend()
     
     
@@ -31,7 +31,7 @@ def plot_hist2d(specifics, fhist2d):
             
     
     g = ax.scatter(colat2d,long2d,c=freq2d, marker='o', edgecolors='none', label=specifics["pol_name"], cmap='inferno')
-    cbar = fig.colorbar(g, label="Frequency")
+    cbar = fig.colorbar(g, label="Count")
 
     
     ax.set_title("Colatitude and Longitude Error Distribution")
