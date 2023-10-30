c6929aabaf9391471dcb7e839ba9e38ece625c51
Matteo Di Mario <mdima0012@gmail.com>
2023-10-30 16:58:38 +0100

    Removed deprecated functions and tests

--- pointing-simulation/Functions.jl
+++ pointing-simulation/Functions.jl
@@ -10,72 +10,6 @@ using ProgressMeter
 # Also some deprecated functions.
 # =========================
 
-function make_histogram(θ, nbins)
-    
-    H = zeros(Float64, 2, nbins)
-    step = 4*θ / nbins  # Range of hist / nbins
-
-    for i in range(1,nbins)
-        H[2,i] = -2*θ + (2*i - 1)/2 * step
-    end
-
-    return (H, step)
-
-end
-
-
-function make_hist(nbins, err_min, err_max)
-
-    H = zeros(Float64, 2, nbins)
-    step = (err_max-err_min) / nbins
-
-    for i in range(1,nbins)
-        H[2,i] = err_min + (2*i - 1)/2 * step
-    end
-
-    return (H, step)
-end
-
-function fill_histogram!(H, step, point_errs)
-
-    outliers = 0
-
-    for i in range(1, length(point_errs))
-        
-        jl = 1
-        jr = length(H[2,:])
-        outliers += 1
-
-        while jl <= jr
-
-            jm = jl + (jr-jl) ÷ 2 # Integer division
-            lbin = H[2,jm] - step/2.
-            rbin = H[2,jm] + step/2.
-
-            if lbin <= point_errs[i] < rbin
-                H[1,jm] += 1
-                outliers -= 1
-                break
-            elseif rbin <= point_errs[i]
-                jl = jm + 1
-            else
-                jr = jm - 1
-            end
-        end
-    end
-
-    return outliers
-end
-
-function print_hist(H, step, outliers, fpath)
-
-    open(fpath, "w") do file
-        @printf(file, "%u\t%.10f\t%u\n", length(H[2,:]), step, outliers)
-        for i in range(1, length(H[2,:]))
-            @printf(file, "%u\t%.10f\n", H[1,i], H[2,i])
-        end
-    end
-end
 
 function telescope_motors(time_s)
    return  (0.0, deg2rad(20.0), Stripeline.timetorotang(time_s, 1.))
@@ -119,45 +53,6 @@ function angle_diff(a,b)
     return dif - 180
 end
 
-function rescaling(point_err)
-
-    err_ave = mean(point_err)
-    point_err_rescaled = (point_err .- err_ave) ./ (1. / 3600.) # Resclaed over arsec
-    return point_err_rescaled
-
-end
-
-
-function set_first_hist!(first_day, time_range, pol_or, nbins, config_ang, outliers)
-
-    dirs_ideal, _ = Stripeline.genpointings(
-        telescope_motors,
-        pol_or,
-        time_range,
-        first_day;
-        config_ang = nothing
-    )
-        
-    dirs_real, _ = Stripeline.genpointings(
-        telescope_motors,
-        pol_or,
-        time_range,
-        first_day;
-        config_ang = config_ang
-    )
-
-    point_err = compute_point_err(dirs_ideal, dirs_real)
-    point_err ./ (1 / 3600.) # scaled to arcsec
-    # point_err = rescaling(point_err)
-
-    err_min = minimum(point_err)
-    err_max = maximum(point_err)
-
-    (H, step) = make_hist(nbins, err_min, err_max)
-    outliers += fill_histogram!(H, step, point_err)
-
-    return (H, step)
-end
 
 function simulate_pointing(params, config_ang_dict, config_ang, start_day, ndays, pol_name)
     
--- pointing-simulation/SimTests.jl
+++ pointing-simulation/SimTests.jl
@@ -6,35 +6,6 @@ using Test
 using Random
 
 @testset "StripeLine-Pointing-Simulation.jl Tests" begin
-    
-        @testset "Histogram Tests" begin
-            
-            θ = 2
-            nbins = 4  
-            step = 4*θ / nbins
-            H = [ [0,-3.0] [0,-1.0 ] [0,1.0] [0,3.0] ]
-
-            (H_t, step_t) = make_histogram(θ, nbins)
-            
-            # Test make_histogram
-            @test nbins == length(H_t[1,:]) 
-            @test H == H_t
-            @test step == step_t
-
-            # Elem in each bin: 3 - 1 - 4 - 2 and two numbers out of bounds (outliers)
-            point_errs = [-4, -2.64, -3.36, -1, 0, 1.5, 1.8, 0.765, 2.56, 3.2, -5, 6]
-            shuffle!(point_errs)
-            outliers_t = fill_histogram!(H_t, step_t, point_errs)
-
-            # Test fill_histogram
-            @test H_t[1,1] == 3
-            @test H_t[1,2] == 1
-            @test H_t[1,3] == 4
-            @test H_t[1,4] == 2
-            @test outliers_t == 2
-
-        end
-
 
         @testset "Pointing Tests" begin
 
@@ -63,9 +34,6 @@ using Random
             pol_name = "I0"
 
             # @test ispath(set_sim_dir(dirname, pol_name, false))
-
-
-
 
         end
 
