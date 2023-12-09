024184ed0b292c5fbb3a92e6fa9a7681955d2f31
Matteo Di Mario <mdima0012@gmail.com>
2023-12-09 17:05:26 +0100

    .

--- data-analysis/BCVDistribution.py
+++ data-analysis/BCVDistribution.py
@@ -1,5 +1,35 @@
 from DataHandling import *
+from PlotOptions import *
 
-df = pd.read_csv("bcv_angles_all_arcsec.csv")
+fname = "data-analysis/bcv_angles_all_arcsec.csv"
 
-print(df)
+df = pd.read_csv(fname, names=["ang", "freq"], header=None).dropna()
+df.ang =  pd.to_numeric(df.ang)
+
+# df = df.astype({"ang": "int", "freq": "int"})
+df = df.groupby(['ang'], as_index=False).sum()
+
+plt.hist(df.ang, bins=20, histtype="step", color=COLORS["palatinate-blue"])
+
+# hist = df.to_numpy()
+
+# # Sort (bins,freq) for hist plotting  
+# hist = np.array(sorted(hist, key=lambda x: x[0]))
+# bins = hist[:,0]
+# freq = hist[:,1]
+
+# bins_edges = np.append(bins-0.5, bins[-1]+0.5)
+
+# fig = plt.figure(figsize=(8,6))
+# ax = plt.gca()
+
+
+# ax.stairs(freq, bins_edges, edgecolor=COLORS["palatinate-blue"], linewidth=1.0, fill=False, label="BCV Angles")
+    
+# title = f"BCV Flexures Angles Distribution"
+# ax.set_title(title)
+# ax.set_xlabel(f"Angle [arcsec]")
+# ax.set_ylabel('Count')
+# ax.legend()
+
+plt.show()
